e91eb0f0ce42e25b969675d8ecfed3ea23cd26a6
macke <marcus.h.olsson@gmail.com>
2013-04-24 22:45:01 +0200

    fixed style

--- js/filters.js
+++ js/filters.js
@@ -5,7 +5,7 @@
 angular.module('partymote.filters', []).
   filter('zero', function() {
   	return function(index){
-  		var s = "0" + index;
-    	return s.substr(s.length-2); 
+  		var s = "00" + index;
+    	return s.substr(s.length-3); 
 	};
   });
