b27bbd840a34695d6a54ebde9c63ac571d64b87b
macke <marcus.h.olsson@gmail.com>
2013-08-11 20:08:53 +0200

    add users track befores app

--- js/services.js
+++ js/services.js
@@ -36,16 +36,18 @@ angular.module('locationService',[])
          	var postPosition = function (position) {
                  	callbackfunc(position);
          	};
-         	var onError = function() {
+         	var onError = function(a,b) {
                 	 console.log("Error getting position");
+                     console.log(a); console.log(b);
          	};
          	return {
                  	start : function(callback){
                          	callbackfunc = callback;
-                         	require(['$api/location#Location'], function(location){
-									location.query().load(['latitude', 'longitude']).done(function(loc) {
+                         	require(['$api/location'], function(location){
+									var loc = location.Location.query();
+                      loc.load(['latitude', 'longitude']).done(function(loc) {
 									  callbackfunc(loc);
-									}).fail(function(a, b) { console.log(a); console.log(b);});;
+									}).fail(onError);;
                          	
                  			});
                          }
@@ -57,8 +59,9 @@ angular.module('locationService',[])
 angular.module('partymote.services',[])
     .service('playlistServices',function($rootScope){
     	var playlist = {tracks:[]};
-    	var loadedPlaylist;
-        var _Track, _Player;
+        var _loadedPlaylist;
+    	  var loadedPlayer;
+        var _Track, _models, _lastSnapshot;
         var index = 0;
         var playerEventListeners = [];
         var playlistEventListeners = [];
@@ -80,39 +83,69 @@ angular.module('partymote.services',[])
         };
 
         var getCurrentTrackInfo = function(){
-            if(_Player && _Player.track){
-                return _Player.track.name;
+            if(_models && loadedPlayer && loadedPlayer.track){
+                return loadedPlayer.track.name;
             }
             return "Nothing playing!"
         };
+         var setIndex = function(e){
+            console.log("index");
+            console.log(e);
+        }
 
+        var getPlaylistID = function(uri){
+          if(!uri) return;
+
+          var index = uri.lastIndexOf(":");
+           return uri.substring(index);
+        };
         var startPlay = function(){
-            if(!_Player.context || _Player.context.uri !=  loadedPlaylist.uri){
-                _Player.playContext(loadedPlaylist);
+            //if(_loadedPlaylist.tracks)
+
+            if(!loadedPlayer.context || getPlaylistID(loadedPlayer.context.uri) !=  getPlaylistID(_loadedPlaylist.uri)){
+                console.log("start play");
+                
+                loadedPlayer.playContext(_loadedPlaylist, 0).done(function(){});
+                
             }else{
-                index = _Player.index;
+                index = loadedPlayer.index;
             }
         };
 
-        var setIndex = function(e){
-            console.log(e);
-        }
+       
       
         var updatePlaylistView = function(){
-            
-            var t = loadedPlaylist.tracks.sort('name:d');
-            console.log(t);
-            console.log(loadedPlaylist);
+            setTimeout(startPlay,500);
+            //var t = _loadedPlaylist.tracks.sort('name:d');
+            //console.log(t);
+            console.log(_loadedPlaylist);
+            console.log(loadedPlayer);
 
-            index = _Player.index;
-            loadedPlaylist.tracks.snapshot().done(function(snapshot) {
+            index = loadedPlayer.index;
+            _loadedPlaylist.tracks.snapshot(index, 50 + index).done(function(snapshot) {
+                         _lastSnapshot = snapshot;
                          playlist.tracks = snapshot.toArray();
                          console.log("time for update");
                          //console.log(playlist.tracks);
                          $rootScope.$apply();
                          playlistChanged(playlist);
-                         startPlay();
+                         
                     });
+
+        };
+
+        var lastAddedIndex = 0;
+        var addUserTrack = function(loadedTrack){
+          if(_lastSnapshot){
+            lastAddedIndex = lastAddedIndex > index ? lastAddedIndex : index;
+            lastAddedIndex = lastAddedIndex + 1;
+            var ref = _lastSnapshot.ref(lastAddedIndex);
+            console.log(lastAddedIndex);
+            console.log(ref);
+            _loadedPlaylist.tracks.insert(ref, loadedTrack);
+          }else{
+            _loadedPlaylist.tracks.add(loadedTrack);
+          }
 
         };
 
@@ -122,16 +155,19 @@ angular.module('partymote.services',[])
                 .load('name','uri','image')
                 .done(function(loadedTrack){
                     loadedTrack.test = 4;
-                    loadedPlaylist.tracks.add(loadedTrack);
+                    //_loadedPlaylist.tracks.add(loadedTrack);
+                    addUserTrack(loadedTrack);
                 });
             };
+
+        
 
         var addHandler = {};
         addHandler.Track = function(track){
             track
                     .load('name','uri','image')
                     .done(function(loadedTrack){
-                             loadedPlaylist.tracks.add(loadedTrack);
+                             _loadedPlaylist.tracks.add(loadedTrack);
                          });
         };
         addHandler.Playlist = function(playlist){
@@ -142,7 +178,8 @@ angular.module('partymote.services',[])
                    dragedPlaylist.tracks.snapshot().done(function(snap){
                         
                         snap.loadAll('name','uri','image').done(function(loadedTracks) {
-                          loadedPlaylist.tracks.add(loadedTracks);
+                          console.log(_loadedPlaylist);
+                          _loadedPlaylist.tracks.add(loadedTracks);
                             });
                     });
                    
@@ -182,24 +219,60 @@ angular.module('partymote.services',[])
 
         };
 
-    	require(['$api/models#Playlist', '$api/models#Track', '$api/models#player'], function(Playlist, Track, Player) {
-            _Track = Track;
-            _Player = Player;
+        var loadPlaylist = function(playlist){
+            playlist.load('tracks').
+                            done(function(playlistPromise){
+                               _loadedPlaylist = playlistPromise;
+                              // _loadedPlaylist.tracks.clear().done();
+                               // loadedPlayer.playContext(_loadedPlaylist, 0);
+                               // var track = Track.fromURI("spotify:track:3fllpI9uZKkdy3NJS0J1oV");
+                               // _loadedPlaylist.tracks.add(track);
+                               // _loadedPlaylist.tracks.remove(track).done();
+                           });
+        };
 
-            _Player.addEventListener('change:index', updatePlaylistView);
-            _Player.addEventListener('change:index', setIndex);
-            _Player.addEventListener('change:track', trackChanged);
+      var test = function(models){
+        models.session.load('user').done(function(blob){
+          console.log(blob);
+          blob.user.load('name').done(function(user){
+            console.log(user);
+          })
+        });
+      }
+        
+    	require(['$api/models'], function(models) {
+            test(models);
+            index = 0;
+            _models = models;
+            _Track = models.Track;
+            models.player.load("index").done(function(player) {
+                loadedPlayer = player;
+                loadedPlayer.addEventListener('change:index', updatePlaylistView);
+                loadedPlayer.addEventListener('change:track', trackChanged);
+            });
+
+
 
             var timeStamp = new Date().getTime();
-    		Playlist.
+    		models.Playlist.
+          //fromURI("spotify:user:macke8080:playlist:3I9xZFEm8CvBJcL55JfXeM").load('name').
     			createTemporary("partymote:"+timeStamp).
     			done(function(p){
-    					   p.load('tracks').
+                                p.addEventListener('insert', updatePlaylistView);
+                                //loadedPlaylist = p.load('tracks');
+                                 //models.Playlist.fromURI(p.uri).load('name').done(function(playlist){
+                                    
+                                    loadPlaylist(p);
+                              //  });
+
+    					  /* p.load('tracks').
     					   	done(function(playlistPromise){
-    					   		loadedPlaylist = playlistPromise;
-                                loadedPlaylist.addEventListener('insert', updatePlaylistView);
-                                updatePlaylistView();
-    					   });
+    					   		_loadedPlaylist = playlistPromise;
+                               // loadedPlayer.playContext(_loadedPlaylist, 0);
+                               // var track = Track.fromURI("spotify:track:3fllpI9uZKkdy3NJS0J1oV");
+                               // _loadedPlaylist.tracks.add(track);
+                               // _loadedPlaylist.tracks.remove(track).done();
+    					   });*/
     					});
     	});
 
